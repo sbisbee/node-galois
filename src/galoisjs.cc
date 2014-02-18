4f0167dcc092618d9176fbad8d44a0f3ade976e6
Sam Bisbee <sam@sbisbee.com>
2014-02-18 16:59:05 -0500

    Single div and simple logs

--- src/galoisjs.cc
+++ src/galoisjs.cc
@@ -27,9 +27,85 @@ Handle<Value> single_multiply(const Arguments& args) {
   return scope.Close(num);
 }
 
+Handle<Value> single_divide(const Arguments& args) {
+  HandleScope scope;
+  Local<Number> num;
+
+  if(args.Length() < 3) {
+    ThrowException(Exception::TypeError(String::New("Wrong number of args")));
+    return scope.Close(Undefined());
+  }
+
+  if(!args[0]->IsNumber() || !args[1]->IsNumber() || !args[2]->IsNumber()) {
+    ThrowException(Exception::TypeError(String::New("Wrong arguments")));
+    return scope.Close(Undefined());
+  }
+
+  num = Number::New(
+    ::galois_single_divide(
+      args[0]->ToInteger()->Value(),
+      args[1]->ToInteger()->Value(),
+      args[2]->ToInteger()->Value()));
+
+  return scope.Close(num);
+}
+
+Handle<Value> log(const Arguments& args) {
+  HandleScope scope;
+  Local<Number> num;
+
+  if(args.Length() < 2) {
+    ThrowException(Exception::TypeError(String::New("Wrong number of args")));
+    return scope.Close(Undefined());
+  }
+
+  if(!args[0]->IsNumber() || !args[1]->IsNumber()) {
+    ThrowException(Exception::TypeError(String::New("Wrong arguments")));
+    return scope.Close(Undefined());
+  }
+
+  num = Number::New(
+    ::galois_log(
+      args[0]->ToInteger()->Value(),
+      args[1]->ToInteger()->Value()));
+
+  return scope.Close(num);
+}
+
+Handle<Value> ilog(const Arguments& args) {
+  HandleScope scope;
+  Local<Number> num;
+
+  if(args.Length() < 2) {
+    ThrowException(Exception::TypeError(String::New("Wrong number of args")));
+    return scope.Close(Undefined());
+  }
+
+  if(!args[0]->IsNumber() || !args[1]->IsNumber()) {
+    ThrowException(Exception::TypeError(String::New("Wrong arguments")));
+    return scope.Close(Undefined());
+  }
+
+  num = Number::New(
+    ::galois_ilog(
+      args[0]->ToInteger()->Value(),
+      args[1]->ToInteger()->Value()));
+
+  return scope.Close(num);
+}
+
 static void Init(Handle<Object> exports) {
   exports->Set(String::NewSymbol("single_multiply"),
     FunctionTemplate::New(single_multiply)->GetFunction());
+
+  exports->Set(String::NewSymbol("single_divide"),
+    FunctionTemplate::New(single_divide)->GetFunction());
+
+  exports->Set(String::NewSymbol("log"),
+    FunctionTemplate::New(log)->GetFunction());
+
+  exports->Set(String::NewSymbol("ilog"),
+    FunctionTemplate::New(ilog)->GetFunction());
 }
 
 NODE_MODULE(galoisjs, Init)
--- test/galois.js
+++ test/galois.js
@@ -1,23 +1,48 @@
 var nodeunit = require('nodeunit');
-var galois = require('../src/galois.js');
+var gf = require('../src/galois.js');
 
 exports.single_multiply = function(test) {
-  test.strictEqual(typeof galois.single_multiply, 'function');
+  test.strictEqual(typeof gf.single_multiply, 'function');
 
   test.throws(
     function() {
-      galois.single_multiply('a', 7, 4);
+      gf.single_multiply('a', 7, 4);
     },
     TypeError, 'No strings');
 
   test.throws(
     function() {
-      galois.single_multiply(null, 7, 4);
+      gf.single_multiply(null, 7, 4);
     },
     TypeError, 'No null');
 
-  test.strictEqual(galois.single_multiply(3, 7, 4), 9);
-  test.strictEqual(galois.single_multiply(1234567, 2345678, 32), 1404360778);
+  test.strictEqual(gf.single_multiply(3, 7, 4), 9);
+  test.strictEqual(gf.single_multiply(1234567, 2345678, 32), 1404360778);
+
+  test.done();
+};
+
+exports.single_divide = function(test) {
+  test.strictEqual(typeof gf.single_divide, 'function');
+  test.strictEqual(gf.single_divide(9, 3, 4), 7);
+  test.strictEqual(gf.single_divide(1404360778, 1234567, 32), 2345678);
+  test.done();
+};
+
+exports.simpleLogs = function(test) {
+  // Prove that: a * b = ilog[ log[a] + log[b] ]
+  var w = 3; //GF8
+  var a = 4;
+  var b = 5;
+
+  var left;
+  var right;
+
+  left = gf.single_multiply(a, b, w);
+  test.strictEqual(left, 2, 'Left');
+
+  right = gf.ilog(gf.log(a, w) + gf.log(b, w), w);
+  test.strictEqual(right, left, 'Proof');
 
   test.done();
 };
