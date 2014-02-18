0ddc8f3bb38c1947272e3965d15a13e90a0f5525
Sam Bisbee <sam@sbisbee.com>
2014-02-18 15:24:30 -0500

    Make sure we're throwing

--- test/galois.js
+++ test/galois.js
@@ -7,9 +7,22 @@ exports.bwah = function(test) {
 };
 
 exports.single_multiply = function(test) {
-  test.expect(3);
   test.strictEqual(typeof galois.single_multiply, 'function');
+
+  test.throws(
+    function() {
+      galois.single_multiply('a', 7, 4);
+    },
+    TypeError, 'No strings');
+
+  test.throws(
+    function() {
+      galois.single_multiply(null, 7, 4);
+    },
+    TypeError, 'No null');
+
   test.strictEqual(galois.single_multiply(3, 7, 4), 9);
   test.strictEqual(galois.single_multiply(1234567, 2345678, 32), 1404360778);
+
   test.done();
 };
